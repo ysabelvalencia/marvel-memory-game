325d3794d829f6f9bc8ad9cd9f34962e153f90d9
Ysabel Valencia <ysavalencia@hotmail.com>
2024-03-17 21:13:24 +0100

    adding more dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import reactLogo from '../assets/react.svg';
 import viteLogo from '/vite.svg';
-import '../styles/App.css';
+import '../styles/App.scss';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { HashRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>
+);
